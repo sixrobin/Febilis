4e57d9f98fd3ba39432998c6679b7ea8afaa878b
Six Robin <six.robin@hotmail.fr>
2021-05-22 00:51:50 +0200

    UI ramp shader WIP, still not working with masked elements.

--- SOURCES/TEMPLAR/Assets/Scripts/PaletteSelector.cs
+++ SOURCES/TEMPLAR/Assets/Scripts/PaletteSelector.cs
@@ -4,17 +4,23 @@
 
     public class PaletteSelector : MonoBehaviour
     {
+        private const string RAMP_TEX_SHADER_PARAM = "_RampTex";
+
         [SerializeField] private Texture2D[] _ramps = null;
+        [SerializeField] private Material _uiRampMat = null;
+
+        private Texture2D _initUIRampTex;
 
         private int _currRampIndex = -1;
-
         public int CurrRampIndex
         {
             get => _currRampIndex;
             private set
             {
                 _currRampIndex = RSLib.Helpers.Mod(value, _ramps.Length);
+
                 Manager.GameManager.PlayerCtrl.CameraCtrl.GrayscaleRamp.OverrideRamp(_ramps[_currRampIndex]);
+                _uiRampMat.SetTexture(RAMP_TEX_SHADER_PARAM, _ramps[_currRampIndex]);
             }
         }
 
@@ -31,12 +37,21 @@
         {
             if (CurrRampIndex == -1)
                 CurrRampIndex = System.Array.IndexOf(_ramps, Manager.GameManager.PlayerCtrl.CameraCtrl.GrayscaleRamp.TextureRamp);
+
+            _initUIRampTex = _uiRampMat.GetTexture(RAMP_TEX_SHADER_PARAM) as Texture2D;
         }
 
         private void Update()
         {
+            // [TOOD] Remove this and do a debug panel.
             if (Input.GetKeyDown(KeyCode.F1))
                 CurrRampIndex++;
+        }
+
+        private void OnDestroy()
+        {
+            // Reset material when leaving play mode. Still need to check out how it's behaving with scenes loading.
+            _uiRampMat.SetTexture(RAMP_TEX_SHADER_PARAM, _initUIRampTex);
         }
     }
 }
---
+++ SOURCES/TEMPLAR/Assets/Scripts/Test/TextureMaker.cs
@@ -0,0 +1,80 @@
+﻿namespace Templar
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class TextureMaker : MonoBehaviour
+    {
+        [System.Serializable]
+        public class ColorReplacement
+        {
+            public Color baseColor;
+            public Color newColor;
+        }
+
+        [SerializeField] private int textureCreationCount = 100;
+        [SerializeField] private UnityEngine.UI.Image _image = null;
+        [SerializeField] private ColorReplacement[] _replacements = null;
+
+        private Dictionary<Color, Color> _palette;
+
+        private void CreateTexture()
+        {
+            Texture2D initTex = _image.sprite.texture;
+            int w = initTex.width;
+            int h = initTex.height;
+
+            Texture2D newTex = new Texture2D(w, h, initTex.format, false)
+            {
+                name = $"{initTex.name}_copy",
+                filterMode = FilterMode.Point,
+                wrapMode = initTex.wrapMode
+            };
+
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    Color sourceColor = initTex.GetPixel(x, y);
+                    Color? targetColor = null;
+
+                    foreach (KeyValuePair<Color, Color> replace in _palette)
+                    {
+                        if (Mathf.Approximately(sourceColor.r, replace.Key.r)
+                            && Mathf.Approximately(sourceColor.g, replace.Key.g)
+                            && Mathf.Approximately(sourceColor.b, replace.Key.b))
+                        {
+                            targetColor = replace.Value;
+                            break;
+                        }
+                    }
+
+                    newTex.SetPixel(x, y, targetColor ?? Color.red);
+                }
+            }
+
+            newTex.Apply();
+
+            _image.overrideSprite = Sprite.Create(
+                newTex,
+                new Rect(0, 0, newTex.width, newTex.height),
+                _image.sprite.pivot,
+                _image.sprite.pixelsPerUnit);
+        }
+
+        private void Start()
+        {
+            _palette = new Dictionary<Color, Color>();
+            foreach (ColorReplacement replacement in _replacements)
+                _palette.Add(replacement.baseColor, replacement.newColor);
+
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+
+            for (int i = 0; i < textureCreationCount; ++i)
+                CreateTexture();
+
+            sw.Stop();
+            Debug.Log($"{sw.ElapsedMilliseconds} ms.");
+        }
+    }
+}
