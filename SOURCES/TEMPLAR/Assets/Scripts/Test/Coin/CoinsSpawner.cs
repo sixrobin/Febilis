9d135138b535241575264105495d5d435a3d5bbe
Six Robin <six.robin@hotmail.fr>
2021-05-27 15:30:44 +0200

    More coin physics.

---
+++ SOURCES/TEMPLAR/Assets/Scripts/Test/Coin/CoinsSpawner.cs
@@ -0,0 +1,22 @@
+﻿namespace Templar
+{
+    using RSLib.Extensions;
+    using UnityEngine;
+
+    public class CoinsSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _coinPrefab = null;
+
+        private void SpawnCoins(Vector3 pos, int count)
+        {
+            for (int i = 0; i < count; ++i)
+                Instantiate(_coinPrefab, pos, _coinPrefab.transform.rotation);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+                SpawnCoins(UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition).WithZ(0f), Random.Range(3, 5));
+        }
+    }
+}
