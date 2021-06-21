0e27397410f94b3735d016fab98ae4d95a2385b3
Six Robin <six.robin@hotmail.fr>
2021-06-22 00:12:33 +0200

    Inventory implementation begin.

---
+++ SOURCES/TEMPLAR/Assets/Scripts/Item/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Templar.Item
+{
+    public abstract class Item
+    {
+        public Item(string id, ItemType type)
+        {
+            Id = id;
+            Type = type;
+        }
+
+        public string Id { get; private set; }
+        public ItemType Type { get; private set; }
+    }
+}
---
+++ SOURCES/TEMPLAR/Assets/Scripts/Item/ItemType.cs
@@ -0,0 +1,11 @@
+﻿namespace Templar.Item
+{
+    public enum ItemType
+    {
+        NONE = 0,
+        QUEST = 1,
+        WEAPON = 2,
+        LORE = 4
+        // etc.
+    }
+}
