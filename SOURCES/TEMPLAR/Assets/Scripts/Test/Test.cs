beeece5baf28acc6c56a9913ae570f56e9a53ddd
Robin Six <six.robin@hotmail.fr>
2022-01-16 20:59:42 +0100

    refs and scene dirty?

--- SOURCES/TEMPLAR/Assets/Scripts/Test/Test.cs
+++ SOURCES/TEMPLAR/Assets/Scripts/Test/Test.cs
@@ -7,10 +7,16 @@
     public class Test : MonoBehaviour
     {
         public RSLib.Framework.Collections.FixedSizedConcurrentQueue<int> queue;
+        public RSLib.Dynamics.Float dynamicFloat;
 
         private void Start()
         {
             queue = new RSLib.Framework.Collections.FixedSizedConcurrentQueue<int>(3);
+
+            dynamicFloat += 66;
+            float t = 0;
+            t += dynamicFloat;
+            Debug.Log(t);
         }
 
         private void Update()
