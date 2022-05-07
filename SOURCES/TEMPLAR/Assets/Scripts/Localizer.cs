0a83f0eabd6dc78614c296e7a6cd39814f2c80f8
Robin Six <55784799+sixrobin@users.noreply.github.com>
2022-05-07 13:17:25 +0200

    Localizer debug class + summaries.

--- SOURCES/TEMPLAR/Assets/Scripts/Localizer.cs
+++ SOURCES/TEMPLAR/Assets/Scripts/Localizer.cs
@@ -9,12 +9,23 @@
 
         private Dictionary<string, Dictionary<string, string>> _entries;
         
+        /// <summary>
+        /// All languages handled in loaded CSV file.
+        /// </summary>
         public string[] Languages { get; private set; }
         
+        /// <summary>
+        /// Currently selected language.
+        /// </summary>
         public string Language { get; private set; }
 
         private static Dictionary<string, string> LanguageEntries => Instance._entries[Instance.Language];
 
+        /// <summary>
+        /// Gets the localized key for current language, and returns the key itself if it has not been found.
+        /// </summary>
+        /// <param name="key">Key to localize.</param>
+        /// <returns>Localized key if it exists, else the key itself.</returns>
         public static string Get(string key)
         {
             if (LanguageEntries.TryGetValue(key, out string entry))
@@ -24,11 +35,21 @@
             return key;
         }
         
+        /// <summary>
+        /// Checks if the key exists for current language, and localizes it if so.
+        /// </summary>
+        /// <param name="key">Key to localize.</param>
+        /// <param name="entry">Localized entry if key exists.</param>
+        /// <returns>True if the key exists, else false.</returns>
         public static bool TryGet(string key, out string entry)
         {
             return LanguageEntries.TryGetValue(key, out entry);
         }
         
+        /// <summary>
+        /// Sets the current language, based on its index in the handled languages list.
+        /// </summary>
+        /// <param name="languageIndex">Selected language index.</param>
         public static void SetCurrentLanguage(int languageIndex)
         {
             if (languageIndex > Instance._entries.Count - 1)
@@ -40,18 +61,26 @@
             SetCurrentLanguage(Instance._entries.ElementAt(languageIndex).Key);
         }
         
-        public static void SetCurrentLanguage(string language)
+        /// <summary>
+        /// Sets the current language, based on its name.
+        /// </summary>
+        /// <param name="languageName">Selected language name.</param>
+        public static void SetCurrentLanguage(string languageName)
         {
-            if (!Instance._entries.ContainsKey(language))
+            if (!Instance._entries.ContainsKey(languageName))
             {
-                Instance.LogWarning($"Tried to set language to {language} but it has not been found!");
+                Instance.LogWarning($"Tried to set language to {languageName} but it has not been found!");
                 return;
             }
 
-            Instance.Log($"Setting language to {language}.");
-            Instance.Language = language;
+            Instance.Log($"Setting language to {languageName}.");
+            Instance.Language = languageName;
         }
         
+        /// <summary>
+        /// Loads a CSV file and parses its data to generate localization dictionaries.
+        /// </summary>
+        /// <param name="csvFile">CSV localization file.</param>
         private static void LoadCSV(UnityEngine.TextAsset csvFile)
         {
             string[,] grid = CSVReader.SplitCSVGrid(csvFile.text);
@@ -59,7 +88,7 @@
             // Initialize languages.
             Instance._entries = new Dictionary<string, Dictionary<string, string>>();
             List<string> languages = new List<string>();
-            for (int x = 1; x < grid.GetLength(0); ++x)
+            for (int x = 1; x < grid.GetLength(0); ++x) // Start at 1 to avoid keys column.
             {
                 string language = grid[x, 0];
                 if (string.IsNullOrEmpty(language))
@@ -100,10 +129,6 @@
             
             if (Instance._entries.Count > 0)
                 Instance.Language = Instance._entries.ElementAt(0).Key;
-            
-            RSLib.Debug.Console.DebugConsole.OverrideCommand(new RSLib.Debug.Console.Command<string>("localizeKey", "Localizes a given key.", (key) => Log(Get(key), forceVerbose: true)));
-            RSLib.Debug.Console.DebugConsole.OverrideCommand(new RSLib.Debug.Console.Command<string>("setLanguage", "Set language.", SetCurrentLanguage));
-            RSLib.Debug.Console.DebugConsole.OverrideCommand(new RSLib.Debug.Console.Command<int>("setLanguageIndex", "Set language index.", SetCurrentLanguage));
         }
     }
 }
---
+++ SOURCES/TEMPLAR/Assets/Scripts/LocalizerDebugger.cs
@@ -0,0 +1,20 @@
+﻿namespace Templar
+{
+    using UnityEngine;
+
+    [DisallowMultipleComponent]
+    public sealed class LocalizerDebugger : MonoBehaviour
+    {
+        private void Awake()
+        {
+            RSLib.Debug.Console.DebugConsole.OverrideCommand(new RSLib.Debug.Console.Command<string>("locKey", "Localizes a given key.",
+                (key) => Localizer.Instance.Log(Localizer.Get(key), forceVerbose: true)));
+            
+            RSLib.Debug.Console.DebugConsole.OverrideCommand(new RSLib.Debug.Console.Command<string>("locSetLanguage", "Set language.",
+                Localizer.SetCurrentLanguage));
+            
+            RSLib.Debug.Console.DebugConsole.OverrideCommand(new RSLib.Debug.Console.Command<int>("locSetLanguageIndex", "Set language index.",
+                Localizer.SetCurrentLanguage));
+        }
+    }
+}
