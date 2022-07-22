6a729d59687b5f8b212f6a5ec874bff1f6f70682
Robin Six <55784799+sixrobin@users.noreply.github.com>
2022-07-22 22:59:04 +0200

    Coins SFX + lock awake fix

--- SOURCES/TEMPLAR/Assets/Scripts/ContextualConditions/ContextualConditionsHandler.cs
+++ SOURCES/TEMPLAR/Assets/Scripts/ContextualConditions/ContextualConditionsHandler.cs
@@ -18,7 +18,8 @@
 
         private void OnInventoryContentChanged(Item.InventoryController.InventoryContentChangedEventArgs args)
         {
-            Check();
+            if (!args.OnInit)
+                Check();
         }
 
         [ContextMenu("Check")]
@@ -82,6 +83,7 @@
             _conditionsDatas = Database.ContextualConditionsDatabase.ContextualConditionsDatas[_id];
             CreateConditionsCheckers();
 
+            _onAnyConditionUnchecked?.Invoke(); // Consider all conditions are false on start, then check them.
             Check(); // Reset values so that we don't have to do it in the editor each time.
         }
 
